d2f7c48b4484a89d1c8ef28c6b3981249f8d48c8
Mrs. Mercedes <mayf218@outlook.com>
2025-10-17 23:14:01 -0400

    First half of Weapon.cs

--- Objects/Weapons/Weapon.cs
+++ Objects/Weapons/Weapon.cs
@@ -1,8 +1,4 @@
-﻿/*
- * Generated code file by Il2CppInspector - http://www.djkaty.com - https://github.com/djkaty
- */
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
@@ -22,18 +18,16 @@ using VampireSurvivors.Objects.Characters;
 using VampireSurvivorsDecompProject.Objects.Pools;
 using VampireSurvivorsDecompProject.Objects.Projectiles;
 
-// Image 2: VampireSurvivors.Runtime.dll - Assembly: VampireSurvivors.Runtime, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null - Types 13826-18866
-
 namespace VampireSurvivorsDecompProject.Objects.Weapons
 {
-	public class Weapon : Equipment // TypeDefIndex: 16805
+	public class Weapon : Equipment
 	{
 		// Fields
 		[FormerlySerializedAs("_projectilePrefab")]
 		[SerializeField]
-		private Projectile _ProjectilePrefab; // 0x68
-		protected GameManager _gameMan; // 0x70
-		protected PlayerOptions _playerOptions; // 0x78
+		private Projectile _ProjectilePrefab;
+		protected GameManager _gameMan;
+		protected PlayerOptions _playerOptions;
 		protected GameSessionData _gameSessionData; // 0x80
 		protected WeaponData _currentWeaponData; // 0x88
 		protected bool _skipAddingEvolution; // 0x90
@@ -70,30 +64,29 @@ namespace VampireSurvivorsDecompProject.Objects.Weapons
 		private static readonly ProfilerMarker _markerCleanup; // 0x00
 		private static readonly ProfilerMarker _markerFireOneProjectile; // 0x08
 	
-		// Properties
 		protected virtual int ProjectilePoolSize { get => default; } // 0x0000000182111EF0-0x0000000182111F00 
 		public PhysicsGroup ProjectileGroup { get => default; } // 0x00000001827666D0-0x00000001827666E0 
 		public List<Projectile> SpawnedProjectiles { get => default; } // 0x0000000181971630-0x0000000181971640 
 		public GameManager GameMan { get => default; } // 0x00000001826E99F0-0x00000001826E9A00 
-		protected HitVfxType VfxType { get => default; } // 0x00000001870CAEA0-0x00000001870CAEC0 
+		protected HitVfxType VfxType { get => _currentWeaponData?.hitVFX ?? HitVfxType.Default; }
 		protected virtual bool UseOnlineTimer { get => default; } // 0x0000000181958370-0x0000000181958380 
 		public float StatsInflictedDamage { get; set; } // 0x0000000186560970-0x0000000186560980 0x0000000186560980-0x0000000186560990
 		public float StatsLifetime { get; private set; } // 0x0000000186758A90-0x0000000186758AA0 0x0000000186758AA0-0x0000000186758AB0
-		public virtual float Chance { get => default; } // 0x00000001870CAEC0-0x00000001870CAEF0 
-		public int Penetrating { get => default; protected set {} } // 0x00000001870CAF30-0x00000001870CAF60 0x00000001870CAF60-0x00000001870CAF90
-		public float Interval { get => default; set {} } // 0x0000000186FA1B70-0x0000000186FA1BA0 0x00000001870CAF90-0x00000001870CAFC0
-		protected float Duration { get => default; } // 0x00000001870CAFC0-0x00000001870CB040 
-		public float RepeatInterval { get => default; } // 0x00000001870CB040-0x00000001870CB070 
+		public virtual float Chance { get => _currentWeaponData.chance; }
+		public int Penetrating { get => _currentWeaponData.penetrating; protected set { _currentWeaponData.penetrating = value; } }
+		public float Interval { get => _currentWeaponData.interval; set { _currentWeaponData.interval = value; } }
+		protected float Duration { get => _currentWeaponData.duration.GetValueOrDefault(1000.0f); }
+		public float RepeatInterval { get => _currentWeaponData.repeatInterval; } // 0x00000001870CB040-0x00000001870CB070 
 		public WeaponData CurrentWeaponData { get => default; } // 0x00000001819A03E0-0x00000001819A03F0 
-		public float HitBoxDelay { get => default; } // 0x00000001870CB070-0x00000001870CB0F0 
-		public float Knockback { get => default; } // 0x00000001870CB0F0-0x00000001870CB170 
+		public float HitBoxDelay { get => _currentWeaponData.hitBoxDelay.GetValueOrDefault(1000.0f); } // 0x00000001870CB070-0x00000001870CB0F0 
+		public float Knockback { get => _currentWeaponData.knockback.GetValueOrDefault(5.0f); } // 0x00000001870CB0F0-0x00000001870CB170 
 		public PlayerOptions PlayerOptions { get => default; } // 0x00000001819A0360-0x00000001819A0370 
 		public bool CanCrit { get; protected set; } // 0x0000000182917560-0x0000000182917570 0x00000001870CB170-0x00000001870CB180
 		public List<float> CritChancesArray { get => default; } // 0x0000000182A45A70-0x0000000182A45A80 
 		public float FreezeChance { get; set; } // 0x0000000184F54B50-0x0000000184F54B60 0x000000018628F1E0-0x000000018628F1F0
 		public virtual float DefangChance { get => default; set {} } // 0x0000000184F54B90-0x0000000184F54BA0 0x0000000186758AB0-0x0000000186758AC0
 		public int CritIndex { get => default; set {} } // 0x0000000181BDC430-0x0000000181BDC440 0x0000000181BDC440-0x0000000181BDC450
-		protected Vector2 PlayerPos { get => default; } // 0x00000001870CB180-0x00000001870CB1C0 
+		protected Vector2 PlayerPos { get => Owner.position.ToVector2(); } // 0x00000001870CB180-0x00000001870CB1C0 
 		public float TotalTime { get; set; } // 0x0000000185F859C0-0x0000000185F859D0 0x0000000185F859D0-0x0000000185F859E0
 		public int LimitBreakLevel { get; private set; } // 0x00000001870CB1C0-0x00000001870CB1D0 0x00000001870CB1D0-0x00000001870CB1E0
 		public bool SkipAddingEvolution { get => default; set {} } // 0x00000001819643B0-0x00000001819643C0 0x00000001826FAF20-0x00000001826FAF30
@@ -120,8 +113,17 @@ namespace VampireSurvivorsDecompProject.Objects.Weapons
 	
 		// Methods
 		public override bool IsPowerup() => default; // 0x0000000180B15290-0x0000000180B152A0
-		public virtual float StatsGetDps() => default; // 0x00000001870CAEF0-0x00000001870CAF30
-		protected override void FakeConstruct() {} // 0x00000001870CB1E0-0x00000001870CB560
+		public virtual float StatsGetDps() => (StatsLifetime == 0.0f) ? 0.0f : StatsInflictedDamage / StatsLifetime;
+		protected override void FakeConstruct()
+		{
+			_dataManager = GM.Core.DataManager;
+			_signalBus = GM.Core.SignalBus;
+			_levelUpFactory = GM.Core.LevelUpFactory;
+			_gameMan = GM.Core;
+			_playerOptions = GM.Core.PlayerOptions;
+			_gameSessionData = GM.Core.GameSessionData;
+			_projectileFactory = GM.Core.ProjectileFactory;
+		}
 		protected virtual new void Awake() 
 		{
 			_cachedTransform = transform;
@@ -129,7 +131,15 @@ namespace VampireSurvivorsDecompProject.Objects.Weapons
 			_wallsColliders = new List<Collider>();
 			accumulatedLimitBreaks = new LimitBreakData();
 		}
-		protected override void OnDestroy() {} // 0x00000001870CB710-0x00000001870CB840
+		protected override void OnDestroy()
+		{
+			if (_firingAnimEvent != null) _firingAnimEvent.Cancel();
+			if (_firingTimer != null) _firingTimer.Cancel();
+			if (_projectilePool != null) _projectilePool.Destroy();
+			_projectilePool = null;
+			if (_secondaryPool != null) _secondaryPool.Destroy();
+			_secondaryPool = null;
+		}
 		public virtual void InitWeapon(VampireSurvivors.Objects.Characters.CharacterController characterController, WeaponType weaponType)
 		{
 			FakeConstruct();
@@ -150,13 +160,63 @@ namespace VampireSurvivorsDecompProject.Objects.Weapons
 		public virtual void OnMirrorData(Vector2 position) {} // 0x0000000180B15170-0x0000000180B15180
 		public virtual void OnWeaponAdded() {} // 0x0000000180B15170-0x0000000180B15180
 		public virtual float CalculateTotalDamage() => default; // 0x0000000186560970-0x0000000186560980
-		protected virtual void OnStart() {} // 0x00000001870CBA50-0x00000001870CC270
+		protected virtual void OnStart()
+		{
+			if (GetFiringAnimation != null) PlayNextAttackAnim();
+			ResetFiringTimer();
+
+			var enemies = _gameMan.Enemies;
+			var overlapEnemy = ArcadePhysics.scene.physics.add.overlap(_projectilePool, enemies, OnBulletOverlapsEnemy);
+			overlapEnemy.setName("Projectiles>Enemies");
+            var overlapDestructibles = ArcadePhysics.scene.physics.add.overlap(_projectilePool, _gameMan.PhysicsManager._destructiblesGroup, OnBulletOverlapsDestructible);
+            overlapDestructibles.setName("Projectiles>Destructibles");
+
+			if (GM.Core.IsLocalMultiplayer)
+			{
+                var overlapPlayers = ArcadePhysics.scene.physics.add.overlap(_projectilePool, _gameMan.PhysicsManager._playerGroup, OnBulletOverlapsPlayer);
+                overlapPlayers.setName("Projectiles>Players");
+            }
+
+			if (!_currentWeaponData.hitsWalls) return;
+
+			if (!_gameMan.Stage.HasTileSet) return;
+			var tiling = _gameMan.Stage.TilingTileset;
+
+			if (tiling == null) return;
+
+			var tileMaps = _gameMan.Stage.TilingTileset.GetPhaserTilemaps();
+			var world = ArcadePhysics.s_world;
+			var setCollider = new TilemapSetCollider(world, false, _projectilePool, null, collideCallback: OnBulletOverlapsWall);
+
+			setCollider.setName("Projectiles>Tilemap");
+			foreach (var tile in tileMaps)
+			{
+				setCollider.AddTilemap(tile._parentSetID, tile);
+			}
+
+			if (ArcadePhysics.scene?.physics?.world != null)
+			{
+				ArcadePhysics.scene.physics.world.addColliderDirect(setCollider);
+			}
+			else
+			{
+				world.addColliderDirect(setCollider);
+			}
+        }
 		public virtual float2 GetFiringVector() => default; // 0x00000001870CC270-0x00000001870CC2B0
 		protected virtual bool OnBulletOverlapsEnemy(CallbackContext context, ArcadeColliderType second, ArcadeColliderType first) => default; // 0x00000001870CC2B0-0x00000001870CC3D0
 		protected virtual bool OnBulletOverlapsPlayer(CallbackContext context, ArcadeColliderType second, ArcadeColliderType first) => default; // 0x00000001870CC3D0-0x00000001870CC640
 		protected virtual bool OnSecondaryBulletOverlapsEnemy(CallbackContext context, ArcadeColliderType second, ArcadeColliderType first) => default; // 0x00000001870CC640-0x00000001870CC7E0
 		protected virtual bool OnSecondaryBulletOverlapsEnemyCurse(CallbackContext context, ArcadeColliderType second, ArcadeColliderType first) => default; // 0x00000001870CC7E0-0x00000001870CC980
-		protected virtual bool OnBulletOverlapsEnemyRetaliation(CallbackContext context, ArcadeColliderType second, ArcadeColliderType first) => default; // 0x000000018706F9E0-0x000000018706FB00
+		protected virtual bool OnBulletOverlapsEnemyRetaliation(CallbackContext context, ArcadeColliderType second, ArcadeColliderType first)
+		{
+			var enemy = first.gameObject.GetComponent<EnemyController>();
+			if (enemy.IsDead) return false;
+
+			var projectile = second.gameObject.GetComponent<Projectile>();
+			if (!projectile.HasAlreadyHitObject(enemy)) DealDamageRetaliation(enemy);
+			return false;
+		}
 		protected virtual bool OnBulletOverlapsDestructible(CallbackContext context, ArcadeColliderType second, ArcadeColliderType first) => default; // 0x00000001870CC980-0x00000001870CCAE0
 		protected virtual bool OnBulletOverlapsWall(CallbackContext context, ArcadeColliderType bullet, ArcadeColliderType tile) => default; // 0x00000001870CCAE0-0x00000001870CCBD0
 		public override void InternalUpdate() {} // 0x00000001870CCBD0-0x00000001870CCCF0
@@ -171,9 +231,9 @@ namespace VampireSurvivorsDecompProject.Objects.Weapons
 		public virtual void HandlePlayerTeleport(float2 destinationPos) {} // 0x0000000180B15170-0x0000000180B15180
 		public virtual float PArea() => default; // 0x00000001870CD490-0x00000001870CD4E0
 		public virtual int PBounces() => default; // 0x00000001870CD4E0-0x00000001870CD4F0
-		public virtual float PAmount() => default; // 0x0000000186FD21D0-0x0000000186FD2230
+		public virtual float PAmount() => _currentWeaponData.amount + Mathf.Min(10.0f, Owner.PAmount()); // 0x0000000186FD21D0-0x0000000186FD2230
 		public virtual float SecondaryPAmount() => default; // 0x00000001870CD4F0-0x00000001870CD510
-		public virtual float PPower() => default; // 0x0000000186F61F50-0x0000000186F61FD0
+		public virtual float PPower() => Owner.BloodlineDamage + (_currentWeaponData.power * Owner.PPowerFinal());
 		public virtual float SecondaryPPower() => default; // 0x00000001870CD510-0x00000001870CD590
 		public virtual float SecondaryCursePPower() => default; // 0x00000001870CD590-0x00000001870CD640
 		public virtual float PSpeed() => default; // 0x00000001870CD640-0x00000001870CD790
@@ -181,7 +241,7 @@ namespace VampireSurvivorsDecompProject.Objects.Weapons
 		public virtual float PSpeedRepeatInterval() => default; // 0x00000001870CD7E0-0x00000001870CD840
 		public virtual float PInterval() => default; // 0x00000001870CD840-0x00000001870CD900
 		public virtual float PDuration() => default; // 0x00000001870CD900-0x00000001870CDA70
-		public virtual void ParadoxFire() {} // 0x0000000186F29A40-0x0000000186F29A60
+		public virtual void ParadoxFire() => Fire(true);
 		public virtual void Fire() {} // 0x00000001870CDA70-0x00000001870CDA90
 		public virtual void Fire(bool skipTriggers = false /* Metadata: 0x01977E63 */) {} // 0x00000001870CDA90-0x00000001870CDDF0
 		public virtual Projectile FireOneProjectile(Vector2 pos, int index, Transform target = null, BulletPool pool = null) => default; // 0x00000001870CDDF0-0x00000001870CE060
@@ -192,7 +252,7 @@ namespace VampireSurvivorsDecompProject.Objects.Weapons
 		public virtual void DealDamage(IDamageable other, float damage) {} // 0x00000001870CE490-0x00000001870CE550
 		public void DamageAllEnemies(float value) {} // 0x00000001870CE550-0x00000001870CE920
 		public virtual void StandardCritical(ArcadeColliderType second, ArcadeColliderType first) {} // 0x00000001870CE920-0x00000001870CEC00
-		public override void CheckArcanas() {} // 0x0000000186F6A610-0x0000000186F6A620
+		public override void CheckArcanas() => CheckBeginningArcana();
 		public void CheckBeginningArcana() {} // 0x00000001870CEC00-0x00000001870CECE0
 		public bool HasActiveArcanaOfType(ArcanaType arcanaType) => default; // 0x00000001870CECE0-0x00000001870CED50
 		public bool CheckFreeze() => default; // 0x00000001870CED50-0x00000001870CEE90
@@ -211,7 +271,7 @@ namespace VampireSurvivorsDecompProject.Objects.Weapons
 		public virtual float GetChanceFromArray() => default; // 0x00000001870CFF50-0x00000001870CFFF0
 		protected override void MakeLevelOne() {} // 0x00000001870CFFF0-0x00000001870D0790
 		public void ReloadCurrentData() {} // 0x00000001870D0790-0x00000001870D0960
-		protected override Dictionary<WeaponType, JArray> GetDataDictionary() => default; // 0x00000001869D49A0-0x00000001869D49C0
+		protected override Dictionary<WeaponType, JArray> GetDataDictionary() => _dataManager.AllWeaponData;
 		private void ApplyLimitBreakStatsToWeaponStats(LimitBreakData limitBreakData) {} // 0x00000001870D0960-0x00000001870D0980
 	}
 }
